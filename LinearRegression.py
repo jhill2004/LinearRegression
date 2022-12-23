20cd86dde195b5c41c15302b42d9d9db04c0c546
jhill2004 <97457539+jhill2004@users.noreply.github.com>
2022-12-23 13:51:31 -0500

    Add files via upload
    - Added function to help visualize gradient regression algorithm to find linear regression line on a 3d plot
    - started to add the code to create the 3d plot

--- LinearRegression.py
+++ LinearRegression.py
@@ -16,6 +16,12 @@ def dslopeRSS(x,y,slope,intercept):
 def linreg(x,slope,intercept):
     return (slope * x + intercept)
 
+def RSS_3d(slope, intercept, x_list, y_list, cost_func):
+    ret = 0
+    for i in range(len(x_list)):
+        ret = ret + cost_func(x_list[i], y_list[i], slope, intercept)
+    return ret
+
 # Getting coords from user and then putting it into numpy array
 
 print("Enter x and y coordinate points, separated by spaces. Enter 'q' to quit.")
@@ -46,6 +52,8 @@ old_intercept = 0
 old_slope = 1
 new_slope = 1
 new_intercept = 0
+slope_list = [new_slope]
+intercept_list = [new_intercept]
 
 # gradients
 dintRSSsum = 0
@@ -64,6 +72,8 @@ for n in range(max_loops):
 
     new_intercept = old_intercept - (learning_rate * dintRSSsum)
     new_slope = old_slope - (learning_rate * dslopeRSSsum)
+    slope_list.append(new_slope)
+    intercept_list.append(new_intercept)
     dintRSSsum = 0
     dslopeRSSsum = 0
 
@@ -75,8 +85,9 @@ for n in range(max_loops):
 
 
 
-# plotting data with linear regression line to visualize data
+#plotting data with linear regression line to visualize data
 
+#plt.subplot(1,3,1)
 plt.scatter(x,y,s=100, color='red', alpha=0.8)
 plt.plot(x, linreg(x, new_slope, new_intercept), linewidth = 3, color='blue', alpha=0.6)
 plt.style.use('fivethirtyeight')
@@ -88,7 +99,28 @@ plt.ylabel('y')
 plt.annotate(('Theta0 (intercept)', new_intercept), xy=(0.05, 0.95), xycoords='axes fraction')
 plt.annotate(('Theta1 (slope)', new_slope), xy=(0.05, 0.85), xycoords='axes fraction')
 
+
+# plt.subplot(1,3,2)
+# plt.plot(x,RSS())
+
 plt.show()
+
+# fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')
+
+# x_axis = np.array(np.linspace(-50,50,500))
+# z_axis = np.array(np.linspace(-50,50,500))
+# y_axis = lambda x_axis, z_axis:RSS(x[0],y[0],x_axis,z_axis)
+
+# x_axis,z_axis = np.meshgrid(x_axis, z_axis)
+
+
+# ax.plot_surface(x_axis,RSS_3d(x_axis, z_axis, x, y, RSS),z_axis)
+# ax.set_aspect('equal')
+# plt.show()
+
+# print(RSS_3d(10, 10, x, y, RSS))
+
 
 
 
